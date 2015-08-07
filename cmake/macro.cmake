72262d2ca6322049850fc08031a1a112de5a0f0d
Krozark <maxime.barbier1991@gmail.com>
2015-08-08 00:22:51 +0200

    add sfml-raytracing

--- cmake/macro.cmake
+++ cmake/macro.cmake
@@ -9,47 +9,53 @@ include(CMakeParseArguments)
 
 macro(build_library target)
 	cmake_parse_arguments(THIS "" "" "SOURCES;TYPE;DEPENDS;EXTERNAL_LIBS" ${ARGN})
-	
-	if(THIS_TYPE STREQUAL SHARED)
-		set(lib ${target}-shared)
-	elseif(THIS_TYPE STREQUAL STATIC)
-		set(lib ${target}-static)
-	else()
-		message(FATAL_ERROR "No TYPE specify for the build. Please set STATIC or SHARED")
-	endif()
-	
-	add_library(${lib} ${THIS_TYPE} ${THIS_SOURCES})
 
-	set_target_properties(${lib} PROPERTIES
-		OUTPUT_NAME ${target}
-		CLEAN_DIRECT_OUTPUT 1
-		#DEBUG_POSTFIX -d
-		VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
-		SOVERSION ${VERSION_MAJOR}.${VERSION_MINOR}
-	)
-	install(TARGETS ${lib}
-		RUNTIME DESTINATION bin
-		LIBRARY DESTINATION lib 
-		ARCHIVE DESTINATION lib
-	)
+    if(THIS_TYPE)
+        foreach(type ${THIS_TYPE})
+            if(type STREQUAL SHARED)
+                set(sufix shared)
+            elseif(type STREQUAL STATIC)
+                set(sufix static)
+            else()
+                message(FATAL_ERROR "No TYPE specify for the build. Please set STATIC or SHARED")
+            endif()
 
-	if(THIS_DEPENDS)
-		target_link_libraries(${lib} ${THIS_DEPENDS})
-	endif()
-	if(THIS_EXTERNAL_LIBS)
-		target_link_libraries(${lib} ${THIS_EXTERNAL_LIBS})
-	endif()
+            set(lib ${target}-${sufix})
+
+            add_library(${lib} ${type} ${THIS_SOURCES})
+
+            set_target_properties(${lib} PROPERTIES
+                OUTPUT_NAME ${target}
+                CLEAN_DIRECT_OUTPUT 1
+                DEBUG_POSTFIX -d
+                VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
+                SOVERSION ${VERSION_MAJOR}.${VERSION_MINOR}
+            )
+            install(TARGETS ${lib}
+                RUNTIME DESTINATION bin
+                LIBRARY DESTINATION lib 
+                ARCHIVE DESTINATION lib
+            )
+
+            if(THIS_DEPENDS)
+                foreach(d ${THIS_DEPENDS})
+                    target_link_libraries(${lib} ${d}-${sufix})
+                endforeach(d)
+            endif()
+            if(THIS_EXTERNAL_LIBS)
+                target_link_libraries(${lib} ${THIS_EXTERNAL_LIBS})
+            endif()
+        endforeach(type)
+    else()
+        message(FATAL_ERROR "TYPE is not set for the build. Please set TYPE as STATIC or/and SHARED")
+    endif()
 endmacro()
 
 
 macro(deploy)
 	cmake_parse_arguments(THIS "" "" "FILES;DESTINATION" ${ARGN})
 	
-	message(Deploy : ${THIS_FILES})
-	
 	file(GLOB DEPLOY_FILES_AND_DIRS "${THIS_FILES}")
-	
-	message(Deploy : ${DEPLOY_FILES_AND_DIRS})
 	
 	set(FILES_TO_DEPLOY "")
 	set(DIRS_TO_DEPLOY "")
@@ -57,10 +63,8 @@ macro(deploy)
 	foreach(ITEM ${DEPLOY_FILES_AND_DIRS})
 		IF( IS_DIRECTORY "${ITEM}" )
 			LIST( APPEND DIRS_TO_DEPLOY "${ITEM}" )
-			message(DIR ${ITEM})
 		ELSE()
 			LIST( APPEND FILES_TO_DEPLOY "${ITEM}" )
-			message(file ${ITEM})
 		ENDIF()
 	endforeach()
 	INSTALL( FILES ${FILES_TO_DEPLOY} DESTINATION ${THIS_DESTINATION} )
--- ideas/sfml/raycasting.cpp
+++ ideas/sfml/raycasting.cpp
@@ -1,3 +1,10 @@
+/**
+ *
+ * http://deepnight.net/bresenham-magic-raycasting-line-of-sight-pathfinding/#more-499
+ *
+ */
+
+
 #include <iostream>
 #include "time.h"
 #include <cmath>
@@ -5,22 +12,30 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
  
-void CreateMap(sf::Vector2i size, sf::VertexArray& array, vector<vector<int> >& tiles) {
+void CreateMap(sf::Vector2i size, sf::VertexArray& array, vector<vector<int> >& tiles)
+{
     tiles = vector<vector<int> >(size.x, vector<int>(size.y, 1));
-    for(int r=0 ; r<250 ; r++) {
+    for(int r=0 ; r<250 ; r++)
+    {
         int w = rand()%20+42 + (rand()%100==0?160:0);
         int h = rand()%16+40 + (rand()%100==0?120:0);
         int x = rand()%(size.x-8-w) + 4;
         int y = rand()%(size.y-8-h) + 4;
-        for(int i=0 ; i<w ; i++) {
-            for(int j=0 ; j<h ; j++) {
+        for(int i=0 ; i<w ; i++)
+        {
+            for(int j=0 ; j<h ; j++)
+            {
                 tiles[i+x][j+y] = 0;
             }
         }
     }
-    for(int i=0 ; i<size.x ; i++) {
-        for(int j=0 ; j<size.y ; j++) {
-            if(tiles[i][j]==1) {
+
+    for(int i=0 ; i<size.x ; i++)
+    {
+        for(int j=0 ; j<size.y ; j++)
+        {
+            if(tiles[i][j]==1)
+            {
                 int b = rand()%32+70;
                 array.append(sf::Vertex(sf::Vector2f(i, j), sf::Color(b, b, b)));
             }
@@ -28,16 +43,19 @@ void CreateMap(sf::Vector2i size, sf::VertexArray& array, vector<vector<int> >& 
     }
 }
  
-void DrawRay(int x0, int y0, int dirx, int diry, sf::VertexArray& array, vector<vector<int> >& tiles) {
+void DrawRay(int x0, int y0, int dirx, int diry, sf::VertexArray& array, vector<vector<int> >& tiles)
+{
     static const sf::Color col(255, 128, 0);
     static const float powaaaa = 16000.f;
     bool swapXY = abs(diry) > abs(dirx);
-    if(swapXY) {
+    if(swapXY)
+    {
         x0 ^= y0; y0 ^= x0; x0 ^= y0;
         dirx ^= diry; diry ^= dirx; dirx ^= diry;
     }
     bool invY = (dirx < abs(diry));
-    if(invY) {
+    if(invY)
+    {
         dirx *= -1;
         diry *= -1;
     }
@@ -46,12 +64,17 @@ void DrawRay(int x0, int y0, int dirx, int diry, sf::VertexArray& array, vector<
     int ystep = (diry < 0 ? -1 : 1);
     diry = int(abs(diry));
     int x = x0;
-    if(invY) {
-        if(swapXY) {
-            while(tiles[y][x]!=1) {
-                if(tiles[y][x]==0) {
+    if(invY)
+    {
+        if(swapXY)
+        {
+            while(tiles[y][x]!=1)
+            {
+                if(tiles[y][x]==0)
+                {
                     float b = 1.f-(float(((x-x0)*(x-x0)+(y-y0)*(y-y0))/powaaaa));
-                    if(b < 0) {
+                    if(b < 0)
+                    {
                         break;
                     }
                     array.append(sf::Vertex(sf::Vector2f(y, x), sf::Color(int(float(col.r)*b), int(float(col.g)*b), int(float(col.b)*b))));
@@ -64,54 +87,73 @@ void DrawRay(int x0, int y0, int dirx, int diry, sf::VertexArray& array, vector<
                 }
                 x--;
             }
-        } else {
-            while(tiles[x][y]!=1) {
-                if(tiles[x][y]==0) {
+        }
+        else
+        {
+            while(tiles[x][y]!=1)
+            {
+                if(tiles[x][y]==0)
+                {
                     float b = 1.f-(float(((x-x0)*(x-x0)+(y-y0)*(y-y0))/powaaaa));
-                    if(b < 0) {
+                    if(b < 0)
+                    {
                         break;
                     }
                     array.append(sf::Vertex(sf::Vector2f(x, y), sf::Color(int(float(col.r)*b), int(float(col.g)*b), int(float(col.b)*b))));
                     tiles[x][y] = 2; //mark as "rayed"
                 }
                 err -= diry;
-                if(err < 0) {
+                if(err < 0)
+                {
                     y -= ystep;
                     err += dirx;
                 }
                 x--;
             }
         }
-    } else {
-        if(swapXY) {
-            while(tiles[y][x]!=1) {
-                if(tiles[y][x]==0) {
+    }
+    else
+    {
+        if(swapXY)
+        {
+            while(tiles[y][x]!=1)
+            {
+                if(tiles[y][x]==0)
+                {
                     float b = 1.f-(float(((x-x0)*(x-x0)+(y-y0)*(y-y0))/powaaaa));
-                    if(b < 0) {
+                    if(b < 0)
+                    {
                         break;
                     }
                     array.append(sf::Vertex(sf::Vector2f(y, x), sf::Color(int(float(col.r)*b), int(float(col.g)*b), int(float(col.b)*b))));
                     tiles[y][x] = 2; //mark as "rayed"
                 }
                 err -= diry;
-                if(err < 0) {
+                if(err < 0)
+                {
                     y += ystep;
                     err += dirx;
                 }
                 x++;
             }
-        } else {
-            while(tiles[x][y]!=1) {
-                if(tiles[x][y]==0) {
+        }
+        else 
+        {
+            while(tiles[x][y]!=1) 
+            {
+                if(tiles[x][y]==0)
+                {
                     float b = 1.f-(float(((x-x0)*(x-x0)+(y-y0)*(y-y0))/powaaaa));
-                    if(b < 0) {
+                    if(b < 0)
+                    {
                         break;
                     }
                     array.append(sf::Vertex(sf::Vector2f(x, y), sf::Color(int(float(col.r)*b), int(float(col.g)*b), int(float(col.b)*b))));
                     tiles[x][y] = 2; //mark as "rayed"
                 }
                 err -= diry;
-                if(err < 0) {
+                if(err < 0)
+                {
                     y += ystep;
                     err += dirx;
                 }
@@ -133,13 +175,17 @@ int main()
     map.setPrimitiveType(sf::Points);
     vector<vector<int> > tiles;
     CreateMap(sf::Vector2i(800, 450), map, tiles);
-    while(window.isOpen()) {
+    while(window.isOpen())
+    {
         sf::Event e;
-        while(window.pollEvent(e)) {
-            if(e.type == sf::Event::Closed) {
+        while(window.pollEvent(e))
+        {
+            if(e.type == sf::Event::Closed) 
+            {
                 window.close();
             }
-            if(e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Escape) {
+            if(e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Escape)
+            {
                 window.close();
             }
         }
@@ -149,17 +195,22 @@ int main()
         texture.clear();
         array.clear();
         //clear ray map
-        for(int i=0 ; i<800 ; i++) {
-            for(int j=0 ; j<450 ; j++) {
-                if(tiles[i][j] == 2) {
+        for(int i=0 ; i<800 ; i++)
+        {
+            for(int j=0 ; j<450 ; j++)
+            {
+                if(tiles[i][j] == 2)
+                {
                     tiles[i][j] = 0;
                 }
             }
         }
         sf::Vector2f mp(sf::Vector2f(sf::Mouse::getPosition(window))/2.f);
-        if(mp.x >= 0 && mp.y >= 0 && mp.x < 800 && mp.y < 450) {
-            const static int nbrays = 100000;
-            for(int i=0 ; i<nbrays ; i++) {
+        if(mp.x >= 0 && mp.y >= 0 && mp.x < 800 && mp.y < 450)
+        {
+            const static int nbrays = 256;
+            for(int i=0 ; i<nbrays ; i++) 
+            {
                 float a = float(i)*2.f*3.1415926535897932384626433f/float(nbrays);
                 DrawRay(mp.x, mp.y, int(cos(a)*128.f), int(sin(a)*128.f), array, tiles);
             }
