960c80584c7ca1908c97eddf67cfa20f3b7f6828
maxime barbier <maxime.barbier@lamark.fr>
2020-07-03 17:39:40 +0200

    add playlist

--- ideas/python-music/soundplayer.py
+++ ideas/python-music/soundplayer.py
@@ -6,22 +6,30 @@ from tempfile import NamedTemporaryFile
 from pydub.utils import get_player_name
 import time
 from enum import Enum
+import threading
+
+l = logging.getLogger("pydub.converter")
+l.setLevel(logging.DEBUG)
+l.addHandler(logging.StreamHandler())
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+class STATUS(Enum):
+    ERROR = -1
+    STOPPED = 1
+    PLAYING = 2
+    PAUSED = 3
 
 
 class Sound(object):
     FFPLAY_PLAYER = get_player_name()
 
-    class STATUS(Enum):
-        STOPPED = 1
-        PLAYING = 2
-        PAUSED = 3
-        ERROR = 10
-
     def __init__(self, segment):
         self._segment = segment
         self._tmp_file = None
         self._popen = None
-        self._status = self.STATUS.STOPPED
+        self._status = STATUS.STOPPED
 
     def __del__(self):
         if self._popen:
@@ -38,8 +46,11 @@ class Sound(object):
     def _create_popen(self):
         self._popen = subprocess.Popen([self.FFPLAY_PLAYER, "-nodisp", "-autoexit", "-hide_banner", self._tmp_file.name])
 
+    def status(self):
+        return self._status
+
     def play(self):
-        if self._status not in (self.STATUS.STOPPED, self.STATUS.PAUSED):
+        if self._status not in (STATUS.STOPPED, STATUS.PAUSED):
             raise Exception()
 
         if self._tmp_file is None:
@@ -47,28 +58,36 @@ class Sound(object):
 
         if self._popen is None:
             self._create_popen()
-        elif self._status == self.STATUS.PAUSED:
+        elif self._status == STATUS.PAUSED:
             self._popen.send_signal(signal.SIGCONT)
 
-        self._status = self.STATUS.PLAYING
+        self._status = STATUS.PLAYING
 
     def wait(self, timeout=None):
         code = self._popen.wait(timeout=timeout)
         return code
 
     def poll(self):
-        code = self._popen.poll()
-        return code
+        if self._popen:
+            code = self._popen.poll()
+            if code is not None:
+                if code == signal.SIGSTOP:
+                    self._status = STATUS.PAUSED
+                elif code == signal.SIGCONT:
+                    self._status = STATUS.PLAYING
+                else: # code == signal.SIGTERM:
+                    self._status = STATUS.STOPPED
+        return self._status
 
     def pause(self):
-        if self._status != self.STATUS.PLAYING:
+        if self._status != STATUS.PLAYING:
             raise Exception()
 
         self._popen.send_signal(signal.SIGSTOP)
-        self._status = self.STATUS.PAUSED
+        self._status = STATUS.PAUSED
 
     def stop(self):
-        if self._status != self.STATUS.PLAYING:
+        if self._status != STATUS.PLAYING:
             raise Exception()
 
         if self._popen:
@@ -78,35 +97,100 @@ class Sound(object):
         if self._tmp_file:
             self._tmp_file.close()
 
-        self._status = self.STATUS.STOPPED
-
-l = logging.getLogger("pydub.converter")
-l.setLevel(logging.DEBUG)
-l.addHandler(logging.StreamHandler())
+        self._status = STATUS.STOPPED
 
 
-song = AudioSegment.from_ogg("music.ogg")
-
-sound = Sound(song)
-sound2 = Sound(song)
-
-sound.play()
-time.sleep(5)
-
-sound2.play()
-sound.pause()
-time.sleep(5)
-
-sound.play()
-time.sleep(5)
-
-sound.stop()
-time.sleep(5)
-sound2.wait()
+# song = AudioSegment.from_ogg("music.ogg")
+#
+# sound = Sound(song)
+#
+# sound.play()
+# time.sleep(5)
+#
+# sound.pause()
+# time.sleep(5)
+#
+# sound.play()
+# time.sleep(5)
+#
+# sound.stop()
+# time.sleep(5)
 
 
+class Playlist(object):
+    def __init__(self, concurency=1):
+        self._concurency = concurency
+        self._queue_waiting = []
+        self._queue_current = []
+        self._thread = None
+        self._lock = threading.Lock()
+        self._status = STATUS.STOPPED
+
+    def enqueue(self, sound):
+        with self._lock:
+            logger.debug("enqueue %s" % sound)
+            self._queue_waiting.append(sound)
+
+    def clear(self):
+        with self._lock:
+            self._queue_waiting.clear()
+            self._queue_current.clear()
+
+    def pause(self):
+        self._status = STATUS.PAUSED
+        with self._lock:
+            for sound in self._queue_current:
+                sound.pause()
+
+    def stop(self):
+        self._status = STATUS.STOPPED
+        with self._lock:
+            for sound in self._queue_current:
+                sound.stop()
+
+        self.clear()
+
+    def play(self):
+        if self._thread is None:
+            logger.debug("Create playlist Thread")
+            self._thread = threading.Thread(target=self._thread_task, daemon=True)
+            logger.debug("Start playlist Thread")
+            self._thread.start()
+
+        with self._lock:
+            for sound in self._queue_current:
+                sound.play()
+        self._status = STATUS.PLAYING
+
+    def _thread_task(self):
+        logger.debug("In playlist Thread")
+        while self._status != STATUS.STOPPED:
+            if self._status == STATUS.PLAYING:
+                with self._lock:
+                    i = 0
+                    while i < len(self._queue_current):
+                        sound_status = self._queue_current[i].poll()
+                        if sound_status == STATUS.STOPPED:
+                            logger.debug("sound %s has stoped. Remove it", sound)
+                            sound = self._queue_current.pop(i)
+                        else:
+                            i += 1
+
+                    while self._concurency > len(self._queue_current) and len(self._queue_waiting):
+                        sound = self._queue_waiting.pop(0)
+                        logger.debug("Add sound %s", sound)
+                        sound.play()
+                        self._queue_current.append(sound)
+
+            time.sleep(0.1)
+        self._thread = None
 
 
+pl = Playlist(concurency=2)
+pl.enqueue(Sound(AudioSegment.from_wav("coin.wav")))
+pl.enqueue(Sound(AudioSegment.from_ogg("music.ogg")))
+pl.enqueue(Sound(AudioSegment.from_wav("coin.wav")))
+pl.enqueue(Sound(AudioSegment.from_wav("coin.wav")))
 
-
-
+pl.play()
+time.sleep(100)
