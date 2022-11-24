6b1a0e7d51bae422e8ed33e24d701ea0d833d7e0
Maxime Barbier <maxime.barbier1991@gmail.com>
2022-11-24 14:40:00 +0100

    Update smtp_email.py

--- ideas/python/smtp_email.py
+++ ideas/python/smtp_email.py
@@ -3,40 +3,46 @@ from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def send_mail(receivers, object, body, html=None, attachments=None):
-    """
-    you will need to provide some infos such as:
 
-     "email": {
-      "sender": "your email",
-      "password": "password",
-      "host": "smtp.gmail.com",
-      "port": 587,
-      "local_hostname": null
-    },
-    """
+def _get_attachment_file_nameand_type(path: str) -> Tuple[str, str]:
+    base_name = os.path.basename(os.path.basename(path))
+    name = regex.match(EMAIL_ATTACHMENT_REGEX_TEMPLATE_FILE_NAME, base_name).group(
+        "name"
+    )
+    t = name.rsplit(".", 1)[-1]
+    return name, t
+
+
+def send_smtp_mail(
+    receivers: List[str],
+    object: str,
+    body: str,
+    html: str = None,
+    attachments: List[Tuple[str, str]] = None,
+) -> None:
     logger.debug("Creating SMTP connexion")
     # create smtp connection
     smtp = smtplib.SMTP(
-        host=config.email.host,
-        port=config.email.port,
-        local_hostname=config.email.local_hostname,
+        host=config.email.smtp.host,
+        port=config.email.smtp.port,
+        local_hostname=config.email.smtp.local_hostname,
     )
     # Login with your email and password
-    if config.email.password:
+    if config.email.smtp.password:
         smtp.ehlo()
         smtp.starttls()
-        smtp.login(config.email.sender, config.email.password)
+        smtp.login(config.email.sender, config.email.smtp.password)
 
-    logger.debug(f"Preparing e-mail to {receivers}:")
+    logger.info(f"Preparing SMTP e-mail to {receivers}:")
     for receiver in receivers:
-        logger.debug(f"Sending e-mail to {receiver}.")
+        logger.info(f"Sending e-mail to {receiver}.")
         msg_root = MIMEMultipart("related")
         msg_root["Subject"] = object
         msg_root["From"] = config.email.sender
         msg_root["To"] = receiver
         msg_root["Date"] = datetime.datetime.now().strftime("%a, %d %b %Y  %H:%M:%S %Z")
-        # Set the multipart email preamble attribute value. Please refer https://docs.python.org/3/library/email.message.html to learn more.
+        # Set the multipart email preamble attribute value.
+        # Please refer https://docs.python.org/3/library/email.message.html to learn more.
         msg_root.preamble = "====================================================="
 
         # Create a 'alternative' MIMEMultipart object. We will use this object to save plain text format content.
@@ -48,13 +54,8 @@ def send_mail(receivers, object, body, html=None, attachments=None):
             msg_alternative.attach(MIMEText(html, "html"))  # Add html contents
 
         for attachment_path, attachment_data in attachments or []:  # add attachments
-            base_name = os.path.basename(os.path.basename(attachment_path))
-            name = regex.match(
-                EMAIL_ATTACHMENT_REGEX_TEMPLATE_FILE_NAME, base_name
-            ).group("name")
-            attachment = MIMEApplication(
-                attachment_data, name=name, _subtype=name.rsplit(".", 1)[-1]
-            )
+            name, subtype = _get_attachment_file_nameand_type(attachment_path)
+            attachment = MIMEApplication(attachment_data, name=name, _subtype=subtype)
             attachment["Content-Disposition"] = f'attachment;filename="{name}"'
             msg_root.attach(attachment)
 
@@ -63,3 +64,65 @@ def send_mail(receivers, object, body, html=None, attachments=None):
             from_addr=config.email.sender, to_addrs=[receiver], msg=msg_root.as_string()
         )
     smtp.quit()
+
+
+def send_mailgun_email(
+    receivers: List[str],
+    object: str,
+    body: str,
+    html: str = None,
+    attachments: List[Tuple[str, str]] = None,
+) -> None:
+    logger.info(f"Preparing mailgun e-mail to {receivers}:")
+    url = urljoin(config.email.mailgun.url, config.email.mailgun.domain + "/messages")
+    auth = ("api", config.email.mailgun.key)
+    files = [
+        (
+            "attachment",
+            (
+                _get_attachment_file_nameand_type(attachment_path)[0],
+                attachment_data,
+            ),
+        )
+        for attachment_path, attachment_data in attachments or []
+    ]
+    data = {"from": config.email.sender, "subject": object, "text": body}
+    if html:
+        data["html"] = html
+
+    logger.debug(f"mailgun url {url}, with {len(files)} attachment")
+
+    for receiver in receivers:
+        logger.info(f"Sending e-mail to {receiver}.")
+        response = requests.post(
+            url,
+            auth=auth,
+            files=files,
+            data={
+                "to": receiver,
+                **data,
+            },
+        )
+        if response.status_code != 200:
+            logger.error(
+                f"Error while querying url '{url}'. {response.status_code}: {response.text}."
+            )
+            response.raise_for_status()
+
+
+def send_mail(
+    receivers: List[str],
+    object: str,
+    body: str,
+    html: str = None,
+    attachments: List[Tuple[str, str]] = None,
+) -> None:
+    if config.email.mailgun.is_valid():
+        send_mailgun_email(receivers, object, body, html=html, attachments=attachments)
+    elif config.email.smtp.is_valid():
+        send_smtp_mail(receivers, object, body, html=html, attachments=attachments)
+    else:
+        msg = "No mail backend configured"
+        logger.error(msg)
+        raise ValueError(msg)
+
